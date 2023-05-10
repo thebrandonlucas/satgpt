93788f7d2117e77e483b309d5882ab7b800d0755
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-05-10 07:10:42 -0400

    feat: add example curls for testing

--- server/curls.sh
+++ server/curls.sh
@@ -7,5 +7,5 @@ curl -X POST \
 # Checking payment (if success, you'll get a chatgpt response)
 curl -X POST \
      -H "Content-Type: application/json" \
-     -d '{"query": "What is the capital of France?", "r_hash": "{hex_r_hash_here}"}' \
+     -d '{ "r_hash": "23cb5236d9bc32e2b90e9926608af26173e02f947511d5574615ff22ff26b24e"}' \
      http://localhost:5000/query

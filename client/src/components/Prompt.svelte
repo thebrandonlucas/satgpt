cf43819d71db90255dfa05739b16cdf2f0625035
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-05-10 09:00:00 -0400

    feat: create prompt

---
+++ client/src/components/Prompt.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+  let selectedOption = ""; // Holds the selected option value
+
+  // Function to handle dropdown selection
+  function handleOptionSelect(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    selectedOption = target.value;
+  }
+
+  function handleSubmit(event: Event) {
+    event.preventDefault();
+    const input = document.getElementById("input") as HTMLInputElement;
+    const prompt = input.value;
+    console.log(prompt);
+  }
+</script>
+
+<main class="bitcoin-orange-theme">
+  <h1>SatGPT</h1>
+
+  <form on:submit={handleSubmit}>
+    <label for="input">Input:</label>
+    <input type="text" id="input" placeholder="Prompt" />
+
+    <label for="dropdown">Dropdown:</label>
+    <select id="dropdown" on:change={handleOptionSelect}>
+      <option value="">Select an option</option>
+      <option value="GPT-3.5">GPT-3.5</option>
+      <option value="GPT-4">GPT-4</option>
+    </select>
+
+    <button type="submit">Submit</button>
+  </form>
+
+  <p>Selected Option: {selectedOption}</p>
+</main>
+
+<style>
+  main {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #f7931a;
+    color: #ffffff;
+    font-family: Arial, sans-serif;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+
+  label {
+    font-size: 16px;
+  }
+
+  input,
+  select {
+    width: 100%;
+    padding: 10px;
+    font-size: 14px;
+    margin-bottom: 10px;
+    border: none;
+    border-radius: 4px;
+    @apply text-black;
+  }
+
+  p {
+    font-size: 14px;
+    margin-top: 20px;
+  }
+</style>
--- client/src/popup/index.ts
+++ client/src/popup/index.ts
@@ -1,12 +1,14 @@
 import "../app.css";
-import Counter from "../components/Counter.svelte";
+import Prompt from "../components/Prompt.svelte";
 
 const target = document.getElementById("app");
 
 // Inject the Counter component into the DOM at element with id "app"
 async function render() {
-  const { count } = await chrome.storage.local.get({ count: 0 });
-  new Counter({ target, props: { count } });
+  // TODO: store conversation
+  //   const { count } = await chrome.storage.local.get({ count: 0 });
+  //   new Counter({ target, props: { count } });
+  new Prompt({ target });
 }
 
 document.addEventListener("DOMContentLoaded", render);

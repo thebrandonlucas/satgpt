c4b09b7fcb4c958781e76d385806d0cea618be25
Brandon Lucas <38222767+thebrandonlucas@users.noreply.github.com>
2023-05-12 23:24:50 -0400

    chore: add error handling and tracing

--- server/satgpt.py
+++ server/satgpt.py
@@ -1,3 +1,4 @@
+import traceback
 import openai
 from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
@@ -42,79 +43,77 @@ def check_payment(r_hash):
 # standard chatgpt query
 @app.route("/query", methods=["POST"])
 def query_chatbot():
-    # Parse the request data
-    data = request.get_json()
+    try:
+        # Parse the request data
+        data = request.get_json()
 
-    # if query is not in data, return error
-    if "query" not in data and "r_hash" not in data:
-        response = jsonify({"message": "No query provided"})
-        response.status_code = 400
-        print(response)
-        return response
+        # if query is not in data, return error
+        if "query" not in data and "r_hash" not in data:
+            response = jsonify({"message": "No query provided"})
+            response.status_code = 400
+            return response
 
-    # check if we're in the middle of a payment
-    if "r_hash" in data:
-        r_hash = data["r_hash"]
-        (paid, invoice) = check_payment(r_hash)
-        print("invoice", invoice)
-        if paid:
-            # Check that the invoice hasn't been used before
-            if check_invoice_used(r_hash):
+        # check if we're in the middle of a payment
+        if "r_hash" in data:
+            r_hash = data["r_hash"]
+            (paid, invoice) = check_payment(r_hash)
+            if paid:
+                # Check that the invoice hasn't been used before
+                if check_invoice_used(r_hash):
+                    # Return the response to the client
+                    response = jsonify({"message": "Payment already used"})
+                    response.status_code = 400
+                    return response
+                else:
+                    # mark invoice as used
+                    set_invoice_used(r_hash)
+
+                # lookup the query associated with the r_hash
+                query = lookup_query(r_hash)
+
+                # Call the OpenAI API to generate a response
+                completion = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}]
+                )
+
+                # Extract the response text from the API response
+                message = completion.choices[0].message.content.strip()
+
                 # Return the response to the client
-                response = jsonify({"message": "Payment already used"})
-                response.status_code = 400
-                print(response)
+                response = jsonify({"message": message})
+                response.status_code = 200
                 return response
             else:
-                # mark invoice as used
-                set_invoice_used(r_hash)
-
-            # lookup the query associated with the r_hash
-            query = lookup_query(r_hash)
-
-            # Call the OpenAI API to generate a response
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}]
-            )
-
-            # Extract the response text from the API response
-            message = completion.choices[0].message.content.strip()
-
-            # Return the response to the client
-            response = jsonify({"message": message})
-            response.status_code = 200
-            print(response)
-            return response
+                # Return the response to the client
+                response = jsonify(
+                    {
+                        "message": "Payment Required",
+                        "payment_request": invoice["payment_request"],
+                        "memo": invoice["memo"],
+                    }
+                )
+                return response, 402
         else:
+            # If r_hash isn't in request, generate an invoice
+            query = data["query"]
+            # generate an invoice
+            invoice = generate_invoice(query)
             # Return the response to the client
-            response = jsonify(
-                {
-                    "message": "Payment Required",
-                    "payment_request": invoice["payment_request"],
-                    "memo": invoice["memo"],
-                }
-            )
-            print(response)
-            return response, 402
-    else:
-        # If r_hash isn't in request, generate an invoice
-        query = data["query"]
-        # generate an invoice
-        invoice = generate_invoice(query)
-        # Return the response to the client
-        # convert r_hash from base64 to hex because for some reason LND returns it in base64
-        r_hash = base64_to_hex(invoice["r_hash"])
-        # add r_hash and query to database
-        add_r_hash_and_query(r_hash, query)
-        response = jsonify(
-            {
+            # convert r_hash from base64 to hex because for some reason LND returns it in base64
+            r_hash = base64_to_hex(invoice["r_hash"])
+            # add r_hash and query to database
+            add_r_hash_and_query(r_hash, query)
+            response = {
                 "message": "Payment Required",
                 "invoice": invoice["payment_request"],
                 "r_hash": r_hash,
             }
-        )
-        print(response)
-        return response
+            return jsonify(response)
+    except Exception as e:
+        # Return an error response if an exception occurs
+        error_message = f"An error occurred: {str(e)}"
+        traceback.print_exc()  # Optional: Print the traceback for debugging purposes
+        response = {"error": error_message}
 
 
 # TODO: send file data in request
